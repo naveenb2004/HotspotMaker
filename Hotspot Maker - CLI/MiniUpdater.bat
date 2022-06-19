e2a297aa5bf0a9862a7ddd55af1b5b7b96dbc904
naveenb2004 <naveennbalasooriya2004@gmail.com>
2022-06-19 14:30:57 +0530

    updates

--- Hotspot Maker - CLI/MiniUpdater.bat
+++
@@ -1,8 +0,0 @@
-@Echo off
-::working path
-set nowpath=%~dp0
-::fix environmet errors
-set rootpath=%SYSTEMROOT%\System32
-::run update checker invisible
-%rootpath%\wscript.exe "%nowpath%\InvisibleUpdateChecker.vbs" "%nowpath%\MiniUpdaterWorker.bat"
-exit
